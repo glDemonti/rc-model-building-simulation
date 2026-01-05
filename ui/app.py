479f6cab176d86815c51247da42238f384c0ec9a
glDemonti <gianluca.demonti@hslu.ch>
2026-01-05 13:34:43 +0100

    Remove commented-out code for heat pump and cooling machine settings in the UI

--- ui/app.py
+++ ui/app.py
@@ -2290,46 +2290,6 @@ with ui.nav_panel("Einstellungen"):
                 )
 
 
-
-#                 with ui.card():
-#                     ui.card_header("Wärmepumpe und Kühlmaschine Einstellungen")
-#                     # ui.input_text(
-#                     #     id="cop_heating",
-#                     #     label="COP der Wärmepumpe []",
-#                     #     value="3.0",
-#                     #     width="600px",
-#                     #     placeholder="Geben Sie eine Zahl ein",
-#                     # )
-#                     # ui.input_text(
-#                     #     id="cop_cooling",
-#                     #     label="COP der Kühlmaschine []",
-#                     #     value="3.0",
-#                     #     width="600px",
-#                     #     placeholder="Geben Sie eine Zahl ein",
-#                     # )
-#                     ui.input_text(
-#                         id="Co2_emission_factor",
-#                         label="CO2-Emissionsfaktor des Strommixes [kg CO2/kWh]",
-#                         value="0.2",
-#                         width="600px",
-#                         placeholder="Geben Sie eine Zahl ein",
-#                     )
-#                     ui.input_text(
-#                         id="Co2_emission_factor_heating",
-#                         label="CO2-Emissionsfaktor für Heizenergie [kg CO2/kWh]",
-#                         value="0.2",
-#                         width="600px",
-#                         placeholder="Geben Sie eine Zahl ein",
-#                     )
-#                     ui.input_text(
-#                         id="Co2_emission_factor_cooling",
-#                         label="CO2-Emissionsfaktor für Kühlenergie [kg CO2/kWh]",
-#                         value="0.2",
-#                         width="600px",
-#                         placeholder="Geben Sie eine Zahl ein",
-#                     )
-# # endregion
-
 with ui.nav_panel("über"):
     "über diese App"
 
